e51451f871a90f2d917fd301334598c2c1ba3927
Alex Martin <atonymartin20@yahoo.com>
2020-07-03 17:03:12 -0500

    Minor team variable changes

--- backend/data/seeds/00_02_teams.js
+++ backend/data/seeds/00_02_teams.js
@@ -2,24 +2,24 @@ exports.seed = function (knex) {
     return knex('teams').insert([
         {
             id: 1,
-            team_name: 'Fighting Toads',
-            season_id: 1,
+            teamName: 'Fighting Toads',
+            seasonId: 1,
             wins: 0,
             losses: 0,
             ties: 0
         },
         {
             id: 2,
-            team_name: 'Fighting Toads 2',
-            season_id: 2,
+            teamName: 'Fighting Toads 2',
+            seasonId: 2,
             wins: 1,
             losses: 1,
             ties: 1
         },
         {
             id: 3,
-            team_name: 'Fighting Toads 3',
-            season_id: 3,
+            teamName: 'Fighting Toads 3',
+            seasonId: 3,
             wins: 2,
             losses: 2,
             ties: 2
