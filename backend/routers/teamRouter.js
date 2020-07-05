9a3faccb611a5b2edc8e5f74be3842b921357e96
Alex Martin <atonymartin20@yahoo.com>
2020-07-05 11:28:48 -0500

    TeamRouter completed

--- backend/routers/teamRouter.js
+++ backend/routers/teamRouter.js
@@ -41,8 +41,13 @@ router.post('/season', (req, res) => {
     const team = req.body;
     teamModel
         .findTeamsBySeason(team.seasonId)
-        .then(team => {
-            res.json(team);
+        .then(teams => {
+            if(teams.length === 0) {
+                res.status(404).json({ message: 'No team with that seasonId exists' });
+            }
+            else {
+                res.json(teams);
+            }
         })
         .catch(err => {
             res.status(500).json({ error: 'Could not find team with inputed seasonId', err });
@@ -55,8 +60,12 @@ router.post('/name', (req, res) => {
     teamModel
         .findTeamByName(team.teamName)
         .then(team => {
-            console.log(team);
-            res.json(team);
+            if(team.length === 0) {
+                res.status(404).json({ message: 'No team with that name exists' });
+            }
+            else {
+                res.json(team);
+            }
         })
         .catch(err => {
             res.status(500).json({ error: 'Could not find team with inputed name', err });
@@ -66,18 +75,66 @@ router.post('/name', (req, res) => {
 // Insert
 router.post('/', (req, res) => {
     const team = req.body;
-    teamModel
-        .insert(team)
-        .then(team => {
-            res.json(team);
-        })
-        .catch(err => {
-            res.status(500).json({ error: 'Could not add team', err });
-        });
+    if(!team.teamName) {
+        res.status(400).json({ error: 'Team does not have a teamName' });
+    }
+    else if(!team.seasonId) {
+        res.status(400).json({ error: 'Team does not have a seasonId '});
+    }
+    else {
+        teamModel
+            .insert(team)
+            .then(team => {
+                res.json(team);
+            })
+            .catch(err => {
+                res.status(500).json({ error: 'Could not add team', err });
+            });
+    }
 });
 
 // Update
+router.put('/:id', (req, res) => {
+    const { id } = req.params; 
+    const team = req.body;
+    teamModel
+        .update(id, team)
+        .then(updatedTeam => {
+            if(updatedTeam === 0) {
+                res.status(404).json({ message: 'No team with that id exists' });
+            }
+            else {
+                teamModel
+                    .findTeamById(id)
+                    .then(team => {
+                        res.json(team)
+                    })
+                    .catch(err => {
+                        res.status(500).json({ error: 'Could not update team', err });
+                    });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Team cannot be modified', err });
+        });
+})
 
 // Delete
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    teamModel
+        .remove(id)
+        .then(removedTeam => {
+            if(removedTeam === 0) {
+                res.status(404).json({ message: 'No team with that id exists' });
+            }
+            else {
+                res.json({ message: 'Team has been deleted' });
+            };
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Team cannot be removed', err });
+        });
+})
 
 module.exports = router;
