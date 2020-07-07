09a5afa98742453d0f6343d2f99d57b796efaf6e
Alex Martin <atonymartin20@yahoo.com>
2020-07-07 17:19:58 -0500

    User Router completed except for password hashing

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -78,4 +78,59 @@ router.post('/email', (req, res) => {
         });
 });
 
+// Insert
+router.post('/', (req, res) => {
+    const user = req.body;
+    if(!user.email) {
+        res.status(400).json({ error: 'User does not have an email address' });
+    }
+    if(!user.username) {
+        res.status(400).json({ error: 'User does not have a username' });
+    }
+    if(!user.password) {
+        res.status(400).json({ error: 'User does not have a password '});
+    }
+    else {
+        userModel
+            .insert(user)
+            .then(user => {
+                res.json(user);
+            })
+            .catch(err => {
+                if(err.code === "SQLITE_CONSTRAINT") {
+                    res.status(500).json({ error: 'Email or username is already being used', err })
+                }
+                else {
+                    res.status(500).json({ error: 'Could not add user', err });
+                }
+            });
+    }
+});
+
+// Update
+router.put('/:id', (req, res) => {
+    const { id } = req.params; 
+    const user = req.body;
+    userModel
+        .update(id, user)
+        .then(updatedUser => {
+            if(updatedUser === 0) {
+                res.status(404).json({ message: 'No user with that id exists' });
+            }
+            else {
+                userModel
+                    .findUserById(id)
+                    .then(user => {
+                        res.json(user)
+                    })
+                    .catch(err => {
+                        res.status(500).json({ error: 'Could not update user', err });
+                    });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'User cannot be modified', err });
+        });
+})
+
 module.exports = router;
