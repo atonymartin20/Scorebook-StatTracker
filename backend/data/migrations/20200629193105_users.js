11b31a3f1d3eb6f45a333d7f16a16b2828ad90d2
Alex Martin <atonymartin20@yahoo.com>
2020-07-07 17:09:37 -0500

    Update to Users Migration Table

--- backend/data/migrations/20200629193105_users.js
+++ backend/data/migrations/20200629193105_users.js
@@ -2,12 +2,12 @@ exports.up = function (knex, Promise) {
     return knex.schema.createTable('users', users => {  
         users.increments();
         users.string('email', 128).notNullable().unique();
-        users.string('username', 128).unique();
-        users.string('password', 128);
-        users.string('firstName', 128);
-        users.string('lastName', 128);
-        users.string('phone', 128);
-        users.string('googleId', 128);
+        users.string('username', 128).notNullable().unique();
+        users.string('password', 128).notNullable();
+        users.string('firstName', 128).defaultTo('');
+        users.string('lastName', 128).defaultTo('');
+        users.string('phone', 128).defaultTo('');
+        users.string('googleId', 128).defaultTo('');
     })
 };
 
