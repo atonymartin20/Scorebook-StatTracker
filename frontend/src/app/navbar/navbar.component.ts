33a708bdeeb13c747b4202c633b229d651a24bcf
Alex Martin <atonymartin20@yahoo.com>
2020-07-10 13:35:42 -0500

    Navbar logic added

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  title = 'Stat Tracker';
+  activeDropdown = false;
+  login: boolean = false;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  activateDropdown() : void {
+    this.activeDropdown = true;
+  }
+
+  closeDropdown(): void {
+    this.activeDropdown = false;
+  }
 }
