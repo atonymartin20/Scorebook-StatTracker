2d3efa729d4d2dc41035877c3cadc9b84181373a
Alex Martin <atonymartin20@yahoo.com>
2020-07-14 10:54:10 -0500

    Login Component TS started.  Still needs login to work properly.

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email = '';
+  password = '';
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit(): void {
+  }
+
+  login(): void {
+    if(this.email !== '' && this.password !== '') {
+      console.log(this.email, this.password)
+    }
+  }
+
+  loginWithGoogle(): void {
+    this.document.location.href='http://localhost:1111/api/authenticationRouter/google';
   }
 
 }
