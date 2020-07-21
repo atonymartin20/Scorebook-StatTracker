36d8f09527ad341474413cb6e69b19cf1e334b77
Alex Martin <atonymartin20@yahoo.com>
2020-07-20 22:19:11 -0500

    Update user.service.ts

--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry, map } from 'rxjs/operators';
+
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  public userInfo = {};
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // login = (credentials) => {
+  //   console.log(credentials)
+  //   console.log(`${environment.apiUrl}/api/authenticationRouter/login`)
+  //   return this.http.post(`${environment.apiUrl}/api/authenticationRouter/login`, credentials).pipe(
+  //     catchError(this.handleError()))
+  // }
+  
+  login(credentials): Observable<any> {
+    console.log(credentials)
+    console.log(`${environment.apiUrl}/api/authenticationRouter/login`)
+    return this.http.post(`${environment.apiUrl}/api/authenticationRouter/login`, credentials).pipe(
+      catchError(this.handleError('login')))
+  }
+
+  private handleError<T>(operation = 'operation', result? :T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      return of(result as T)
+    }
+  }
 }
+
