12d8ca6a811404e092932cca1245719977f30c81
Alex Martin <atonymartin20@yahoo.com>
2020-07-29 09:41:31 -0500

    Adding Side Bar Component

---
+++ frontend/src/app/side-bar/side-bar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { environment } from 'src/environments/environment';
+
+@Component({
+    selector: 'app-side-bar',
+    templateUrl: './side-bar.component.html',
+    styleUrls: ['./side-bar.component.css'],
+})
+export class SideBarComponent implements OnInit {
+    constructor() {}
+    seasonsInfo = environment.seasonsInfo;
+
+    ngOnInit(): void {
+        console.log(environment);
+        console.log(this.seasonsInfo);
+    }
+}
