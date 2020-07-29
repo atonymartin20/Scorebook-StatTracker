2d67be73135239b2d20416e97eaad0ff6f1709ec
Alex Martin <atonymartin20@yahoo.com>
2020-07-29 09:44:45 -0500

    Update authorize.component.ts

--- frontend/src/app/authorize/authorize.component.ts
+++ frontend/src/app/authorize/authorize.component.ts
@@ -6,40 +6,41 @@ import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
 @Component({
-  selector: 'app-authorize',
-  templateUrl: './authorize.component.html',
-  styleUrls: ['./authorize.component.css']
+    selector: 'app-authorize',
+    templateUrl: './authorize.component.html',
+    styleUrls: ['./authorize.component.css'],
 })
 export class AuthorizeComponent implements OnInit {
-  token = environment.tokenData;
-  
-  constructor(private userService: UserService, private router: Router, private seasonService: SeasonsService) { }
-  
-  ngOnInit(): void {
-    this.grabData(environment.tokenData);
-    // console.log(window.location)
-    // if(window.location.search !== '') {
-    //   environment.tokenData = window.location.search.slice(7,9999);
-    //   this.token = window.location.search.slice(7,9999)
-    //   this.grabData(this.token);
-    // }
-    // else {
-    // }
-  }
+    token = environment.tokenData;
 
+    constructor(private userService: UserService, private router: Router, private seasonService: SeasonsService) {}
 
-  grabData(token): void {
-    console.log(environment)
-    this.userService.grabUserData(token).subscribe((userData:any[]) => {
-      environment.userInfo = userData
-      this.seasonService.grabSeasonData(userData, token).subscribe((seasonData: any[]) => {
-        environment.seasonsInfo = seasonData
-      }, (error) => {
-        console.log(error)
-        environment.seasonsInfo = [];
-      })
-      this.router.navigate(['/dashboard'])
-    })
-  }
+    ngOnInit(): void {
+        this.grabData(environment.tokenData);
+        // console.log(window.location)
+        // if(window.location.search !== '') {
+        //   environment.tokenData = window.location.search.slice(7,9999);
+        //   this.token = window.location.search.slice(7,9999)
+        //   this.grabData(this.token);
+        // }
+        // else {
+        // }
+    }
 
+    grabData(token): void {
+        this.userService.grabUserData(token).subscribe((userData: any[]) => {
+            environment.userInfo = userData;
+            this.seasonService.grabSeasonData(userData, token).subscribe(
+                (seasonData: any[]) => {
+                    environment.seasonsInfo = seasonData;
+                },
+                (error) => {
+                    console.log(error);
+                    environment.seasonsInfo = [];
+                }
+            );
+        });
+        let timeout: number;
+        timeout = window.setTimeout(() => {this.router.navigate(['/dashboard'])}, 500);
+    }
 }
