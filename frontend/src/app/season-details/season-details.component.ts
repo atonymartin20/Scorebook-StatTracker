aab84a27fd86ed74817039bf0eb23f7f3aafa360
Alex Martin <atonymartin20@yahoo.com>
2020-08-06 18:48:39 -0500

    Added edit and delete booleans, team names now showing alphabetically

--- frontend/src/app/season-details/season-details.component.ts
+++ frontend/src/app/season-details/season-details.component.ts
@@ -10,28 +10,54 @@ import { environment } from 'src/environments/environment';
 })
 export class SeasonDetailsComponent implements OnInit {
     teamsInSeason = [];
+    season = [];
     seasonId = environment.activeSeason;
+    edit: boolean = false;
+    delete: boolean = false;
 
     constructor(private teamService: TeamsService) {}
 
     ngOnInit(): void {
+        if (environment.seasonsInfo.find((x) => x.id === environment.activeSeason)) {
+            let foundIndex = environment.seasonsInfo.findIndex((x) => x.id === environment.activeSeason);
+            this.season = environment.seasonsInfo[foundIndex]
+        }
         this.findTeamsInSeason();
+    }
+
+    toggleEdit(): void {
+        this.edit = true;
+    }
+
+    toggleDelete(): void {
+        this.delete = !this.delete;
     }
 
     findTeamsInSeason(): void {
         let seasonData = {
             seasonId: this.seasonId
         }
-        console.log(environment.activeSeason)
         this.teamService.findTeamsBySeasonId(seasonData).subscribe(
             (data: any[]) => {
+                data.sort(function(a, b) {
+                    if(a.teamName < b.teamName) { return -1 }
+                    if (a.teamName > b.teamName) { return 1 }
+                    return 0
+                })
                 environment.teamsInSeason = data;
-                console.log(data)
                 this.teamsInSeason = data;
             },
             (error) => {
                 console.log(error);
             }
         )
+    }
+
+    editSeason(): void {
+        this.edit = false;
+    }
+
+    deleteSeason(): void {
+
     }
 }
