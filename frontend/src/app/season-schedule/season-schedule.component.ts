18e778e16fd9cb66e1a92a9ae9458ae74ac05cbe
Alex Martin <atonymartin20@yahoo.com>
2020-08-08 13:26:25 -0500

    Created Season Schedule Component

---
+++ frontend/src/app/season-schedule/season-schedule.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-season-schedule',
+  templateUrl: './season-schedule.component.html',
+  styleUrls: ['./season-schedule.component.css']
+})
+export class SeasonScheduleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
