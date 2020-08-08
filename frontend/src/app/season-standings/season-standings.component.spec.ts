a7734134b7b65d63b2dc0a9b196884e5ff8dec41
Alex Martin <atonymartin20@yahoo.com>
2020-08-08 13:26:13 -0500

    Created Season Standings Component

---
+++ frontend/src/app/season-standings/season-standings.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SeasonStandingsComponent } from './season-standings.component';
+
+describe('SeasonStandingsComponent', () => {
+  let component: SeasonStandingsComponent;
+  let fixture: ComponentFixture<SeasonStandingsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SeasonStandingsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SeasonStandingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
