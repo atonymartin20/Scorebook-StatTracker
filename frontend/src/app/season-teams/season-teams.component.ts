0423bd2ba1e3f15f8236b8030e5c20e5915b3084
Alex Martin <atonymartin20@yahoo.com>
2020-08-08 13:26:00 -0500

    Created season-teams Component

---
+++ frontend/src/app/season-teams/season-teams.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-season-teams',
+  templateUrl: './season-teams.component.html',
+  styleUrls: ['./season-teams.component.css']
+})
+export class SeasonTeamsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
