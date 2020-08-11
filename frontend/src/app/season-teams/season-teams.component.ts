ed8f8d93743d1486b1dc9e8a22bf5ee117018868
Alex Martin <atonymartin20@yahoo.com>
2020-08-10 21:46:24 -0500

    Update season-teams.component.ts

--- frontend/src/app/season-teams/season-teams.component.ts
+++ frontend/src/app/season-teams/season-teams.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { environment } from 'src/environments/environment';
+
 @Component({
   selector: 'app-season-teams',
   templateUrl: './season-teams.component.html',
   styleUrls: ['./season-teams.component.css']
 })
 export class SeasonTeamsComponent implements OnInit {
-
+  teams = environment.teamsInSeason
   constructor() { }
 
   ngOnInit(): void {
+    console.log(environment)
   }
 
 }
