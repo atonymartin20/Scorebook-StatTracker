7845057404e5cacfcf6071e7ab0e665ca649eca1
Alex Martin <atonymartin20@yahoo.com>
2020-08-12 21:13:54 -0500

    Adding team logic is now working in Season Team Component

--- frontend/src/app/season-teams/season-teams.component.ts
+++ frontend/src/app/season-teams/season-teams.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamsService } from '../teams.service';
+import { Router } from '@angular/router';
 
 import { environment } from 'src/environments/environment';
 
@@ -16,16 +18,19 @@ export class SeasonTeamsComponent implements OnInit {
     teamNameError:boolean = false;
     difference = 0;
     addedTeams = [];
+    disabledButton:boolean  = false;
 
-    constructor() {}
+    constructor(private router: Router, private teamService: TeamsService) {}
 
     ngOnInit(): void {
         if (environment.seasonsInfo.find((x) => x.id === environment.activeSeason)) {
             let foundIndex = environment.seasonsInfo.findIndex((x) => x.id === environment.activeSeason);
             this.season = environment.seasonsInfo[foundIndex];
         }
-        console.log(environment);
-        console.log(this.season['teamCount']);
+        this.teamCheck();
+    }
+
+    teamCheck(): void {
         if(this.season['teamCount'] === this.teams.length) {
             this.equalTeams = true;
             this.lessTeams = false;
@@ -49,5 +54,41 @@ export class SeasonTeamsComponent implements OnInit {
             this.extraTeams = true;
             this.difference = this.teams.length - this.season['teamCount'];
         }
+    }
+
+    addTeams(): void {
+        this.disabledButton = true;
+        console.log(this.addedTeams, this.season['id'])
+        this.addedTeams.map((team) => {
+            let teamData = {
+                teamName: team.name,
+                seasonId: this.season['id']
+            }
+            this.teamService.addTeam(teamData).subscribe(
+                (error) => {
+                    console.log(error);
+                }
+            )
+        })
+        let seasonData = {
+            seasonId: this.season['id']
+        }
+        this.teamService.findTeamsBySeasonId(seasonData).subscribe(
+            (data: any[]) => {
+                data.sort(function(a, b) {
+                    if(a.teamName < b.teamName) { return -1 }
+                    if (a.teamName > b.teamName) { return 1 }
+                    return 0
+                })
+                environment.teamsInSeason = data;
+                this.teams = data;
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+        let timeout: number;
+
+        timeout = window.setTimeout(() => {this.router.navigate(['/season/details'])}, 900);
     }
 }
