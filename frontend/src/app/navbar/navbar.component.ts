babc43e3adf1cdc917299a65e11ab43c5648cf91
Alex Martin <atonymartin20@yahoo.com>
2020-08-16 12:53:15 -0500

    Added logout logic

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { environment } from 'src/environments/environment';
 
@@ -12,7 +13,7 @@ export class NavbarComponent implements OnInit {
     activeDropdown = false;
     token = environment.tokenData;
 
-    constructor() {}
+    constructor(private router: Router) {}
 
     ngOnInit(): void {}
 
@@ -22,5 +23,14 @@ export class NavbarComponent implements OnInit {
 
     closeDropdown(): void {
         this.activeDropdown = false;
+    }
+
+    logoutUser(): void {
+        environment.tokenData = null;
+        environment.seasonsInfo = null;
+        environment.teamsInSeason = null;
+        environment.userInfo = null;
+        let timeout: number;
+        timeout = window.setTimeout(() => {this.router.navigate(['/welcome'])}, 300);
     }
 }
