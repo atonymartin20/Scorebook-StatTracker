fd66934b58ac3c1c1fc61b44e881fc9df0b5f0eb
Alex Martin <atonymartin20@yahoo.com>
2020-08-18 23:31:58 -0500

    Update user-settings.component.ts

--- frontend/src/app/user-settings/user-settings.component.ts
+++ frontend/src/app/user-settings/user-settings.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { environment } from 'src/environments/environment';
+
 @Component({
-  selector: 'app-user-settings',
-  templateUrl: './user-settings.component.html',
-  styleUrls: ['./user-settings.component.css']
+    selector: 'app-user-settings',
+    templateUrl: './user-settings.component.html',
+    styleUrls: ['./user-settings.component.css'],
 })
 export class UserSettingsComponent implements OnInit {
+    constructor() {}
+    userInfo = environment.userInfo;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        // email: 'testemail1@email.com';
+        // firstName: 'tester';
+        // googleId: '';
+        // id: 1;
+        // lastName: 'one';
+        // password: '$2a$04';
+        // phone: '1234567890';
+        // username: 'test1';
+    }
 }
