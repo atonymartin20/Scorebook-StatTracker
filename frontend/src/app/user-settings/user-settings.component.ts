8f4090d75091d02a81362950a9c06262d9a70866
Alex Martin <atonymartin20@yahoo.com>
2020-08-22 22:20:25 -0500

    Not working yet

--- frontend/src/app/user-settings/user-settings.component.ts
+++ frontend/src/app/user-settings/user-settings.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { TeamsService } from '../teams.service';
+import { SeasonsService } from '../seasons.service';
+import { Router } from '@angular/router';
 
 import { environment } from 'src/environments/environment';
 
@@ -9,12 +12,14 @@ import { environment } from 'src/environments/environment';
     styleUrls: ['./user-settings.component.css'],
 })
 export class UserSettingsComponent implements OnInit {
-    constructor(private userService: UserService) {}
+    constructor(private router: Router, private userService: UserService, private teamService: TeamsService, private seasonService: SeasonsService) {}
     userInfo = environment.userInfo;
-    delete:boolean = false;
+    delete: boolean = false;
     edit: boolean = false;
+    seasonsInfo = environment.seasonsInfo;
 
     ngOnInit(): void {
+        console.log(environment);
         // email: 'testemail1@email.com';
         // firstName: 'tester';
         // googleId: '';
@@ -23,17 +28,34 @@ export class UserSettingsComponent implements OnInit {
         // password: '$2a$04';
         // phone: '1234567890';
         // username: 'test1';
+        // seasonsInfo: (5) [{…}, {…}, {…}, {…}, {…}]
+        this.seasonsInfo.map((season) => {
+            console.log(season);
+        });
     }
 
     deleteAccount(): void {
         this.userService.deleteUser(this.userInfo['id']).subscribe(
-            (userData: any[]) => {
-                console.log(userData)
+            () => {
+                this.seasonsInfo.map((season) => {
+                    console.log(season);
+                    this.seasonService.deleteSeason(season['id']).subscribe(() => {
+                        this.teamService.findTeamsBySeasonId(season['id']).subscribe((teamsInSeason: any[]) => {
+                            console.log(teamsInSeason);
+                            teamsInSeason.map((team) => {
+                                console.log(team);
+                                this.teamService.deleteTeam(team['id']).subscribe((error) => {
+                                    console.log(error['error']);
+                                });
+                            });
+                        });
+                    });
+                });
             },
             (error) => {
                 console.log(error);
             }
-        )
+        );
     }
 
     toggleEdit(): void {
