db04fcae717220ad58c940990d82e0e29ff81690
Alex Martin <atonymartin20@yahoo.com>
2020-08-25 08:15:01 -0500

    Added Team Sidebar Component

---
+++ frontend/src/app/team-sidebar/team-sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-team-sidebar',
+    templateUrl: './team-sidebar.component.html',
+    styleUrls: ['./team-sidebar.component.css'],
+})
+export class TeamSidebarComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
