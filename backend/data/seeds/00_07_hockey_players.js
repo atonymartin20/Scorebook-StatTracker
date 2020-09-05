e4cc47e683d6442ab461086532bf33365bf869cd
Alex Martin <atonymartin20@yahoo.com>
2020-09-04 21:55:02 -0500

    Update 00_07_hockey_players.js

--- backend/data/seeds/00_07_hockey_players.js
+++ backend/data/seeds/00_07_hockey_players.js
@@ -1,13 +1,31 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = function (knex) {
+  return knex('hockey-players').insert([
+      {
+          id: 1,
+          firstName: 'Player',
+          lastName: '1',
+          currentTeam: 'Fighting Toads',
+          Spring2019Team: 'Fighting Toads',
+          Spring2019TeamId: 1,
+          Spring2019Games: 1,
+      },
+      {
+          id: 2,
+          firstName: 'Player',
+          lastName: '2',
+          currentTeam: 'Fighting Toads 2',
+          Summer2019Team: 'Fighting Toads 2',
+          Summer2019TeamId: 2,
+          Spring2019Games: 2,
+      },
+      {
+          id: 3,
+          firstName: 'Player',
+          lastName: '3',
+          currentTeam: 'Fighting Toads 3',
+          Fall2019Team: 'Fighting Toads 3',
+          Fall2019TeamId: 3,
+          Fall2019Games: 3,
+      },
+  ]);
 };
