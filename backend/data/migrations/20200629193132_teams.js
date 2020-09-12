3172f0a5a1a74b0a50ccdb73882c31783b2149bb
Alex Martin <atonymartin20@yahoo.com>
2020-09-11 20:57:04 -0500

    Update 20200629193132_teams.js

--- backend/data/migrations/20200629193132_teams.js
+++ backend/data/migrations/20200629193132_teams.js
@@ -2,12 +2,16 @@ exports.up = function (knex, Promise) {
     return knex.schema.createTable('teams', teams => {  
         teams.increments();
         teams.string('teamName', 128).notNullable();
+        teams.string('sport', 128).defaultTo('');
         teams.integer('seasonId').unsigned().notNullable();
         teams.integer('wins').unsigned().defaultTo(0);
         teams.integer('losses').unsigned().defaultTo(0);
         teams.integer('ties').unsigned().defaultTo(0);
         teams.integer('pointsScored').unsigned().defaultTo(0);
         teams.integer('pointsAgainst').unsigned().defaultTo(0);
+        teams.integer('podiums').unsigned().defaultTo(0);
+        teams.integer('top5Finishes').unsigned().defaultTo(0);
+        teams.integer('top10Finishes').unsigned().defaultTo(0);
     })
 };
 
